05a7c8df4c2b7b48f797e1ea8b66d2eef6d4a16f
lohkungByAssary <lohkung@gmail.com>
2018-07-06 12:01:02 +0700

    update practice all

---
+++ WebAppVC/Default.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebAppVC.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script lang="javascript" type="text/html">
+        document.write("Hello JavaScript world");
+
+        $(document).ready(function(){
+            var txt = $("#txtSearch");
+            txt.focus();
+        });
+
+        function SearchPerson() {
+            $.get("SearchProduct.aspx,
+            {   
+                q: $("#txtSearch").val()
+            },
+            function (customer){
+            $("#divPerson").html(customer);
+        
+            });
+            
+     
+
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server" >
+        <div>
+            <asp:TextBox ID="txtData" runat="server"></asp:TextBox>
+            <asp:Button ID="cmdCheck" runat="server" Text="ตรวจสอบ" OnClick="cmdCheck_Click" />
+        </div>
+        <div id="div1" runat="server">Test</div>
+        <asp:ListBox ID="ListBox1" runat="server" AutoPostBack="True" Width="220px">
+            <asp:ListItem>TEST1</asp:ListItem>
+            <asp:ListItem>TEST2</asp:ListItem>
+            <asp:ListItem>TEST3</asp:ListItem>
+            <asp:ListItem>TEST4</asp:ListItem>
+        </asp:ListBox>
+
+        <div>
+            <asp:GridView ID="gvCustomers" runat="server"></asp:GridView>
+        </div>
+
+        <div>
+            <asp:TextBox ID="txtSearch" runat="server"></asp:TextBox>
+            
+        </div>
+
+        <div id="divPerson"></div>
+
+    </form>
+</body>
+</html>
---
+++ WebAppVC/Default.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAppVC
+    
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        practiceEntities edm = new practiceEntities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            for(int i = 1; i<=5; i++)
+            {
+                Response.Write("<font size="+ i + ">Hello ASP.NET 4.6 VC#" + "</font><br/>");
+
+            }
+            div1.Style["color"] = "blue"; 
+
+            if (Page.IsPostBack == true)
+            {
+                Response.Write(txtData.Text);
+
+                Response.Write(ListBox1.SelectedItem.Text);
+            }
+
+            var order = from o in edm.Orders select o;
+            if (order.Count() > 0)
+            {
+                foreach(var o in order.ToList())
+                {
+                    Response.Write(o.OrderNumber);
+                    
+                    Response.Write(o.Person.FirstName);
+
+                    Response.Write("<br/>");
+                }
+               
+            }
+            var obj = from o in edm.Orders
+                      from p in edm.Persons
+                      where o.PersonID == p.ID
+                      select new
+                      {
+                          OrderId = o.OrderID,
+                          PersonName = p.FirstName + " " + p.LastName
+                      };
+
+
+            foreach (var o in obj.ToList())
+            {
+                Response.Write(o.OrderId);
+
+                Response.Write(o.PersonName);
+
+                Response.Write("<br/>");
+            }
+
+        }
+
+        protected void cmdCheck_Click(object sender, EventArgs e)
+        {
+            Response.Write(txtData.Text);
+        }
+    }
+}
